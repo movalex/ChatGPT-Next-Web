29819737412963c8e4e26a0c051d2e2df9919010
Alexey Bogomolov <mail@abogomolov.com>
2024-02-03 03:02:23 +0300

    use gpt4-turbo where possible

--- app/client/api.ts
+++ app/client/api.ts
@@ -11,7 +11,7 @@ import { GeminiProApi } from "./platforms/google";
 export const ROLES = ["system", "user", "assistant"] as const;
 export type MessageRole = (typeof ROLES)[number];
 
-export const Models = ["gpt-3.5-turbo", "gpt-4"] as const;
+export const Models = ["gpt-3.5-turbo", "gpt-4-turbo-preview"] as const;
 export type ChatModel = ModelType;
 
 export interface RequestMessage {
@@ -144,7 +144,7 @@ export function getHeaders() {
   const headers: Record<string, string> = {
     "Content-Type": "application/json",
     "x-requested-with": "XMLHttpRequest",
-    "Accept": "application/json",
+    Accept: "application/json",
   };
   const modelConfig = useChatStore.getState().currentSession().mask.modelConfig;
   const isGoogle = modelConfig.model === "gemini-pro";
